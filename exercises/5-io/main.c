fd044c38b4acc495de5bc86c05e9631cbc30f903
ttlindkvist <thomas.toft@hotmail.com>
2021-02-17 11:02:53 +0100

    finished io

--- exercises/5-io/main.c
+++ exercises/5-io/main.c
@@ -14,7 +14,7 @@ void printheading(){
 }
 
 void readcmd(int argc, char **argv){
-    printf("\nREADING WITH CMD\n");
+    printf("\nREADING WITH CMD - WRITING TO STDOUT\n");
     printheading();
     for(int i = 2; i<argc; i++){
         double x = atof(argv[i]);
@@ -23,7 +23,7 @@ void readcmd(int argc, char **argv){
     printf("-------------------------------------\n");
 }
 void readstdio(){
-    printf("\nREADING WITH STDIN\n");
+    printf("\nREADING WITH STDIN - WRITING TO STDOUT\n");
     printheading();
     double x;
     while(scanf("%lg", &x) > 0){
@@ -32,8 +32,24 @@ void readstdio(){
     printf("-------------------------------------\n");
 
 }
-void readfile(char **argv){
-    
+void readfile(int argc, char **argv){
+    assert(argc >= 4);
+    FILE *in_stream = fopen(argv[2], "r");
+    FILE *out_stream = fopen(argv[3], "a");
+
+    fprintf(out_stream, "\nREADING FROM FILE - WRITING TO FILE\n");
+    fprintf(out_stream, "-------------------------------------\n");
+    fprintf(out_stream, "| x\t| cos(x)\t| sin(x)\t|\n");
+    fprintf(out_stream, "-------------------------------------\n");
+
+    double x;
+    while(fscanf(in_stream, "%lg", &x) > 0){
+        fprintf(out_stream, "| %g\t| %g\t| %g\t|\n", x, cos(x), sin(x));
+    }
+    fprintf(out_stream, "-------------------------------------\n");
+
+    fclose(in_stream);
+    fclose(out_stream);
 }
 
 int main(int argc, char **argv){
@@ -43,9 +59,10 @@ int main(int argc, char **argv){
     } else if(strcmp(argv[1], "STDIN") == 0){
         readstdio();
     } else if(strcmp(argv[1], "FILE") == 0){
-        readfile(argv);
+        readfile(argc, argv);
     } else{
-        fprintf(stderr, "ERROR: only supports reading with argv[1] being \"CMD\", \"STDIO\" or \"FILE\"\n");
+        fprintf(stderr, "ERROR: only supports reading with argv[1] being \"CMD\", \"STDIN\" or \"FILE\"\n");
+        exit(EXIT_FAILURE);
     }
-    return 0;
+    exit(EXIT_SUCCESS);
 }
