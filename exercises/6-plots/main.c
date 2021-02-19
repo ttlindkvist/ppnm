a9fdbe5d293f7b83327e73dcdd7a3838fcda8e84
ttlindkvist <thomas.toft@hotmail.com>
2021-02-19 16:46:50 +0100

    plotting exercise A and B

---
+++ exercises/6-plots/main.c
@@ -0,0 +1,58 @@
+#include<stdio.h>
+#include<math.h>
+#include<complex.h>
+#include<gsl/gsl_sf_erf.h>
+#include<gsl/gsl_sf_gamma.h>
+
+double Erf(double x){
+    /// single precision error function (Abramowitz and Stegun, from Wikipedia)
+    if(x<0) return -Erf(-x);
+    double a[]={0.254829592,-0.284496736,1.421413741,-1.453152027,1.061405429};
+    double t=1/(1+0.3275911*x);
+    double sum=t*(a[0]+t*(a[1]+t*(a[2]+t*(a[3]+t*a[4]))));/* the right thing */
+    return 1-sum*exp(-x*x);
+}
+double Gamma(double x){
+    ///single precision gamma function (Gergo Nemes, from Wikipedia)
+    if(x<0)return M_PI/sin(M_PI*x)/Gamma(1-x);
+    if(x<9)return Gamma(x+1)/x;
+    double lnGamma=x*log(x+1/(12*x-1/x/10))-x+log(2*M_PI/x)/2;
+    return exp(lnGamma);
+}
+double LGamma(double x){
+    ///single precision gamma function (Gergo Nemes, from Wikipedia);
+    if(x<9)return LGamma(x+1)-log(x);
+    double lnGamma=x*log(x+1/(12*x-1/x/10))-x+log(2*M_PI/x)/2;
+    return lnGamma;
+}
+complex CGamma(complex z){
+    return z;
+}
+
+int main(){
+    FILE *erf_out = fopen("erf.dat", "w");
+    FILE *gamma_out = fopen("gamma.dat", "w");
+    FILE *lgamma_out = fopen("lgamma.dat", "w");
+
+    double xmin=-2, xmax=2;
+    for(double x=xmin; x<=xmax; x+=1.0/16){
+        fprintf(erf_out, "%10g %10g %10g %10g\n", x, erf(x), gsl_sf_erf(x), Erf(x));
+    }
+
+    int N = 10000;
+    double eps = 1.0/(N*10);
+    for(int i=0; i<N; i++){
+        double x = -5.0 + 10.0*i/N + eps;
+        fprintf(gamma_out, "%10g %10g %10g %10g\n", x, tgamma(x), gsl_sf_gamma(x), Gamma(x));
+    }
+
+    for(int i=0; i<N; i++){
+        double x = 10.0*i/N + eps;
+        fprintf(lgamma_out, "%10g %10g %10g %10g\n", x, lgamma(x), gsl_sf_lngamma(x), LGamma(x));
+    }
+
+    fclose(erf_out);
+    fclose(gamma_out);
+    fclose(lgamma_out);
+    return 0;
+}
