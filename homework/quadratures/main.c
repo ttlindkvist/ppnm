bb2999c0052e21093b1e580a4d903bb3e3362762
ttlindkvist <thomas.toft@hotmail.com>
2021-04-13 22:18:42 +0200

    cont. work

--- homework/quadratures/main.c
+++ homework/quadratures/main.c
@@ -7,29 +7,70 @@ double sqrt_x(double x){
     fevals++;
     return sqrt(x);
 }
+double one_over_sqrt(double x){
+        fevals++;
+        return 1./sqrt(x);
+}
+double ln_over_sqrt(double x){
+    fevals++;
+    return log(x)/sqrt(x);
+}
 double circle(double x){
     fevals++;
     return 4.*sqrt(1-x*x);
 }
 
 int main(){
+    double eps = 1e-4, abs = 1e-4;
+
+    printf("\n------ PART A ------ \nAll integrals calculated with recursive adaptive integrator\n");
+    printf("Absolute precision %g - Relative precision %g\n", abs, eps);
+
     fevals = 0;
-    double I1 = adapt_quad24(sqrt_x, 0, 1, 1e-5, 1e-5);
-    printf("int sqrt(x) from 0 to 1\nCalculated to \t%.10f\nShould be \t%.10f\nWith error \t%.10f\nwith %d function evaluations\n\n", \
-            I1, 2./3, fabs(2./3-I1), fevals);
+    double I = adapt_quad24(sqrt_x, 0, 1, abs, eps);
+    printf("int sqrt(x) from 0 to 1\nCalculated to \t%.16f\nShould be \t%.16f\nWith error\t%.16f\nwith %d function evaluations\n\n", \
+            I, 2./3, fabs(2./3-I), fevals);
+
+    fevals = 0;
+    I = adapt_quad24(circle, 0, 1, abs, eps);
+    printf("int 4*sqrt(1-x*x) from 0 to 1\nCalculated to \t%.16f\nShould be \t%.16f\nWith error\t%.16f\nwith %d function evaluations\n\n", \
+            I, M_PI, fabs(M_PI-I), fevals);
+
+    fevals = 0;
+    I = adapt_quad24(one_over_sqrt, 0, 1, abs, eps);
+    printf("int  1/sqrt(x) from 0 to 1\nCalculated to \t%.16f\nShould be \t%.16f\nWith error \t%.16f\nwith %d function evaluations\n\n", \
+            I, 2., fabs(2.-I), fevals);
     
     fevals = 0;
-    double I2 = adapt_quad24(circle, 0, 1, __DBL_EPSILON__, __DBL_EPSILON__);
-    printf("int 4*sqrt(1-x*x) from 0 to 1\nCalculated to \t%.20f\nShould be \t%.20f\nWith error \t%.20f\nwith %d function evaluations\n\n", \
-            I2, M_PI, fabs(M_PI-I2), fevals);
-    fevals = 0;
-    double I3 = adapt_clenshaw_curtis(sqrt_x, 0, 1, 1e-5, 1e-5);
-    printf("int sqrt(x) from 0 to 1\nCalculated to \t%.10f\nShould be \t%.10f\nWith error \t%.10f\nwith %d function evaluations\n\n", \
-            I3, 2./3, fabs(2./3-I3), fevals);
+    I = adapt_quad24(ln_over_sqrt, 0, 1, abs, eps);
+    printf("int ln(x)/sqrt(x) from 0 to 1\nCalculated to \t%.16f\nShould be \t%.16f\nWith error \t %.16f\nwith %d function evaluations\n\n", \
+            I, -4., fabs(-4.-I), fevals);
+    
+    printf("\n------ PART B ------\n\n");
     
     fevals = 0;
-    double I4 = adapt_clenshaw_curtis(circle, 0, 1, __DBL_EPSILON__, __DBL_EPSILON__);
-    printf("int 4*sqrt(1-x*x) from 0 to 1\nCalculated to \t%.20f\nShould be \t%.20f\nWith error \t%.20f\nwith %d function evaluations\n\n", \
-            I4, M_PI, fabs(M_PI-I4), fevals);
+    I = adapt_clenshaw_curtis(one_over_sqrt, 0, 1, abs, eps);
+    printf("int 1/sqrt(x) from 0 to 1 - using Clenshaw-Curtis transformation\nCalculated to \t%.16f\nShould be \t%.16f\nWith error \t%.15f\nwith %d function evaluations\n\n", \
+            I, 2., fabs(2.-I), fevals);
+    
+    fevals = 0;
+    I = adapt_clenshaw_curtis(ln_over_sqrt, 0, 1, abs, eps);
+    printf("int ln(x)/sqrt(x) from 0 to 1 - using Clenshaw-Curtis transformation\nCalculated to \t%.16f\nShould be \t%.16f\nWith error \t %.15f\nwith %d function evaluations\n\n", \
+            I, -4., fabs(-4.-I), fevals);
+    
+    printf("As seen the CC transformation results in much fewer function evaluations\n");
+    printf("Also, the error seems to be on the same order or lower (at a first glance)\n");
+
+    printf("\nEvaluating 4*sqrt(1-x*x) with and without Clenshaw-Curtis with machine epsilon as eps and abs tolerance\n");
+    fevals = 0;
+    I = adapt_quad24(circle, 0, 1, __DBL_EPSILON__, __DBL_EPSILON__);
+    printf("int 4*sqrt(1-x*x) from 0 to 1\nCalculated to \t%.16f\nShould be \t%.16f\nWith error \t%.16f\nwith %d function evaluations\n\n", \
+            I, M_PI, fabs(M_PI-I), fevals);
+
+    fevals = 0;
+    I = adapt_clenshaw_curtis(circle, 0, 1, __DBL_EPSILON__, __DBL_EPSILON__);
+    printf("int 4*sqrt(1-x*x) from 0 to 1\nCalculated to \t%.16f\nShould be \t%.16f\nWith error \t%.16f\nwith %d function evaluations\n\n", \
+            I, M_PI, fabs(M_PI-I), fevals);   
+
     return 0;
 }
